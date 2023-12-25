40ccb3c2da3a29c6c1960e1d4bdc970fbe71e275
MinethV <minethvismitha@gmail.com>
2023-12-25 13:49:10 +0530

    first commit

---
+++ ThunderMac Connect/ViewModel/iCloudViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  iCloudViewModel.swift
+//  ThunderMac Connect
+//
+//  Created by Admin on 2023-05-13.
+//
+
+import Foundation
---
+++ ThunderMac Connect/firebase_firestore.swift
@@ -0,0 +1,20 @@
+//
+//  firebase_firestore.swift
+//  ThunderMac Connect
+//
+//  Created by Admin on 2023-05-13.
+//
+
+import SwiftUI
+
+struct firebase_firestore: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct firebase_firestore_Previews: PreviewProvider {
+    static var previews: some View {
+        firebase_firestore()
+    }
+}
